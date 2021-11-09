1fa494230092502f4cc8138e4fe009732dd6f928
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2021-11-10 00:30:57 +0800

    Update le-jail.sh
    Updated from py37 to py38 to address the issue identified at https://www.truenas.com/community/threads/lets-encrypt-with-freenas-11-1-and-later.61156/post-667148

--- le-jail.sh
+++ le-jail.sh
@@ -94,7 +94,7 @@ print_msg "Jail Creation. Installing packages will take a while..."
 cat <<__EOF__ >/tmp/pkg.json
         {
   "pkgs":[
-  "py37-pip","python3","bash","curl","security/ca_root_nss","git-lite"
+  "py38-pip","py38-requests","python3","bash","curl","security/ca_root_nss","git-lite"
   ]
 }
 __EOF__
